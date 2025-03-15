40f28bb1e13349064ea514578cd54ea9824605d5
yoshi0518 <yoshihumi.n.0518@gmail.com>
2025-03-15 20:00:35 +0900

    Auth.jsを設定

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,35 @@
 import type { FC } from 'react';
 import Link from 'next/link';
-import { env } from '@/env';
+import { auth } from '@/auth';
+import { Button } from '@/components/ui/button';
 
-import { HelloWorldLabel } from './_components/hello-world-label';
+const Home: FC = async () => {
+  const session = await auth();
+  console.log({ session });
 
-const Home: FC = () => {
   return (
-    <main>
-      <div className="flex h-screen items-center justify-center">
-        <div className="flex flex-col items-center">
-          <HelloWorldLabel />
-          <p>ENV：{env.ENV}</p>
-          <p>DEBUG：{env.DEBUG}</p>
-          <Link href="/about">About</Link>
+    <main className="flex h-screen flex-col items-center justify-center gap-4">
+      <div>
+        <h1 className="text-2xl font-bold">Auth.js</h1>
+      </div>
+      {!session && (
+        <div>
+          <Button>
+            <Link href="/api/auth/signin">SignIn</Link>
+          </Button>
         </div>
+      )}
+      {session && (
+        <div>
+          <Button>
+            <Link href="/api/auth/signout">SignOut</Link>
+          </Button>
+        </div>
+      )}
+      <div>
+        <pre className="bg-slate-100 p-2 text-sm text-slate-700">
+          <code>{JSON.stringify(session, null, 2)}</code>
+        </pre>
       </div>
     </main>
   );
